26bcd65ef08205c45aad593fbeaa64d1cd47942d
FaisalMashuri <faisal.mashuri18143@student.unsika.ac.id>
2022-11-19 22:00:49 +0700

    mdfe submission 1

---
+++ lib/domain/repositories/tv_series_repository.dart
@@ -0,0 +1,23 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/domain/entities/tv_series.dart';
+import 'package:ditonton/domain/entities/tv_series_detail.dart';
+
+abstract class TvRepository {
+
+  //tv home
+  Future<Either<Failure, List<TvSeries>>> searchTv(String query);
+  Future<Either<Failure, List<TvSeries>>> getNowPlayingTv();
+  Future<Either<Failure, List<TvSeries>>> getPopularTv();
+  Future<Either<Failure, List<TvSeries>>> getTopTv();
+
+  //detail
+  Future<Either<Failure, TvDetail>> getTvDetail(int id);
+  Future<Either<Failure, List<TvSeries>>> getTvRecommendations(int id);
+
+  //wishlist
+  Future<Either<Failure, String>> saveWatchlist(TvDetail tvSeries);
+  Future<Either<Failure, String>> removeWatchlist(TvDetail tvSeries);
+  Future<bool> isAddedToWatchlist(int id);
+  Future<Either<Failure, List<TvSeries>>> getWatchlistTv();
+}
